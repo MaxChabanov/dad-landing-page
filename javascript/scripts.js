3270bb89133d74ef041adcfb061f08e99d5dc296
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-08-14 17:20:15 +0300

    made the slider

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,3 +6,22 @@ $(".arrow-down").click(() => {
     1000
   );
 });
+
+var slider = tns({
+  container: ".slider",
+  nav: false,
+  controlsContainer: "#controls",
+  prevButton: "#slider-nav-prev",
+  nextButton: "#slider-nav-next",
+  controlsPosition: "top",
+
+  responsive: {
+    1000: {
+      items: 2,
+      gutter: 35,
+    },
+    900: {
+      gutter: 20,
+    },
+  },
+});
